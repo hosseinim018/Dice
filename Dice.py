a9845147524d29c291604e668168e37f21592aaa
mahdi hosseini <hosseinim018@gmail.com>
2023-07-30 08:50:41 +0330

    add api id and api hash

--- Dice.py
+++ Dice.py
@@ -1,13 +1,13 @@
 from telethon.sync import TelegramClient, events, Button
 
 api = {
-    'api_id':'',
-    'api_hash':'',
+    'api_id':1719463,
+    'api_hash':'a4517a41ddb3b3c8bd544e45db0a7dc3',
     'bot_token':'968534296:AAG6OR6RNnBKhwLk8DR1b6SjIoSVUpTxu6Y'
 }
 proxy = ("socks5", '127.0.0.1', 10808)
 
-Client = TelegramClient('Dice_session').start(bot_token=api['bot_token'])
+Client = TelegramClient('Dice_session', api['api_id'], api['api_hash']).start(bot_token=api['bot_token'])
 
 @Client.on(events.NewMessage(func=lambda event: event.dice))
 async def handle_dice(event):
