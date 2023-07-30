8914ae2c0d6aeb377ac6b0af351ca1f379ee14dc
mahdi hosseini <hosseinim018@gmail.com>
2023-07-31 00:53:54 +0330

    in player module create and initalize class player

---
+++ Player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self, id, betList):
+        self.amount= {
+            'wallet': 50_000,
+            'bet': 0
+        }
+        self.id = id
+        self.betList = betList
+
+
