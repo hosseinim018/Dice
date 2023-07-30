efb3821461ca61845c6dd7e56d740ae93999d45d
mahdi hosseini <hosseinim018@gmail.com>
2023-07-30 09:47:24 +0330

    fix isusse line 18 dict to dice

--- Dice.py
+++ Dice.py
@@ -15,7 +15,7 @@ Client = TelegramClient('Dice_session', api['api_id'], api['api_hash'], proxy=pr
 async def roll_dice(event):
     dice = InputMediaDice('🎲')
     print(dice)
-    await Client.send_file(event.chat_id, dict)
+    await Client.send_file(event.chat_id, dice)
 
 @Client.on(events.NewMessage(func=lambda event: event.dice))
 async def handle_dice(event):
