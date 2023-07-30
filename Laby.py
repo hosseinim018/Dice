aad1c8551c8360032af8a56f194d988a9724c4af
mahdi hosseini <hosseinim018@gmail.com>
2023-07-30 20:39:20 +0330

    define amount method in laby class in line 8 of Laby.py

--- Laby.py
+++ Laby.py
@@ -4,3 +4,15 @@ class laby:
         pass
     def addPlayer(self, player):
         self.players.append(player)
+
+    def amount(self):
+        """
+            amount return total amount in laby
+            :return a number that is amount
+        :return:
+        """
+        amount = 0
+        for player in self.players:
+            amount += player.bet
+
+        return amount
