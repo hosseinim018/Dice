b5b3b8988978693789096ff7e5bb0473ceaa337e
mahdi hosseini <hosseinim018@gmail.com>
2023-07-30 08:45:03 +0330

    first commit

---
+++ Dice.py
@@ -0,0 +1,19 @@
+from telethon.sync import TelegramClient, events, Button
+
+api = {
+    'api_id':'',
+    'api_hash':'',
+    'bot_token':''
+}
+proxy = ("socks5", '127.0.0.1', 10808)
+
+Client = TelegramClient('Dice_session', int(api['api_id']), api['api_hash']).start(bot_token=api['bot_token'])
+
+@Client.on(events.NewMessage(func=lambda event: event.dice))
+async def handle_dice(event):
+    value = event.message.dice.value
+    print(f"The dice rolled: {value}")
+
+
+with Client:
+    Client.run_until_disconnected()
