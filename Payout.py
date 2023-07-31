97f36c1eb84ee13980f50efcd0d808649ef324b4
mahdi hosseini <hosseinim018@gmail.com>
2023-08-01 00:32:40 +0330

    define checkEvent method in payout class in Payout module in line 5

--- Payout.py
+++ Payout.py
@@ -1,3 +1,13 @@
 class payout:
     def __init__(self):
         pass
+
+    def checkEven(self, num:int)->bool:
+        """
+            this function check a number is even or odd
+            :return boolean if number be even return True else False
+        """
+        if num % 2 == 0:
+            return True
+        else:
+            return False
