bb20aef58470cf3286998bf83c3e0d4cf3eeeb17
mahdi hosseini <hosseinim018@gmail.com>
2023-08-01 01:24:42 +0330

    define checkGusses methon in payout class in line 20

--- Payout.py
+++ Payout.py
@@ -2,7 +2,7 @@ class payout:
     def __init__(self):
         pass
 
-    def checkEven(self, num:int)->bool:
+    def isEven(self, num:int) -> bool:
         """
             this function check a number is even or odd
 
@@ -16,4 +16,36 @@ class payout:
             return True
         else:
             return False
+
+    def checkGusses(self,rolls: tuple):
+        """
+            Determine whether the user has gussed the correct
+            Args:
+                rolls(tuple): rolls have 2 index rolls number 1 and 2
+            Returs:
+                gusses (dict): ---
+        """
+        num_roll1, num_roll2 = rolls
+        # check roll1/2 is even/odd 
+        roll1, roll2 = self.isEven(num_roll1), self.isEven(num_roll2)
+        gusses = {
+            'minimumEven': False,
+            'minimumOdd': False,
+            'maximumEven': False,
+            'maximumOdd': False,
+            'roll1Even': roll1,
+            'roll2Even': roll2,
+            'sumEven': self.isEven(num_roll1 + num_roll2)
+        }
+        # check condition of possible minimum even/odd
+        if roll1 == True or roll2 == True:
+            gusses['minimumEven'] = True
+        if roll1 != False or roll2 != False:
+            gusses['minimumOdd'] = True
+        # check condition of possible maximum even/odd
+        if roll1 == True and roll2 == True:
+            gusses['maximumEven'] = True
+        if roll1 != False and roll2 != False:
+            gusses['maximumOdd'] = True
+
 
