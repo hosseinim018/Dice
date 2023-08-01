3ea16f4515e4f674fd05f8a45bb6ece2d5c13d8e
mahdi hosseini <hosseinim018@gmail.com>
2023-08-01 22:05:51 +0330

    define get_factor method in lobby and import payout class to this mofule and make a argumante rolls in __init__

--- Lobby.py
+++ Lobby.py
@@ -1,7 +1,9 @@
+from Payout import payout
 class Lobby:
     players: list = []
-    def __int__(self):
-        pass
+    def __int__(self, rolls: tuple):
+        self.rolls: tuple = rolls
+
     def addPlayer(self, player):
         self.players.append(player)
         print(f'player {player.id} joined to the laby')
@@ -18,3 +20,10 @@ class Lobby:
             amount += betAmount
 
         return amount
+
+    def get_factors(self):
+        total_factor = 0
+        for player in self.players:
+            pay = payout(self.rolls, player.predict)
+            factor = pay.factor()
+            total_factor += factor
