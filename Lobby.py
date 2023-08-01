a16e8936983a3033ee1d86f268a4004062c1821a
mahdi hosseini <hosseinim018@gmail.com>
2023-08-01 22:29:45 +0330

    create pay method for lobby

--- Lobby.py
+++ Lobby.py
@@ -27,4 +27,14 @@ class Lobby:
             pay = payout(self.rolls, player.betList)
             factor = pay.factor()
             total_factor += factor
-        return  total_factor
+        return total_factor
+
+    def pay(self):
+        factor = self.get_factors()
+        lobby_amount = self.amount()
+        amount = lobby_amount/factor
+        for player in self.players:
+            factor = player.factor()
+            pay = amount * factor
+            player.pay(pay)
+
