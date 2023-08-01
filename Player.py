d43befbd407479dd7daec3d85d82971c456385f8
mahdi hosseini <hosseinim018@gmail.com>
2023-08-01 22:02:18 +0330

    remove validation method in Player class in player.py

--- Player.py
+++ Player.py
@@ -18,51 +18,5 @@ class Player:
     def getBetAmount(self):
         return self.amount['bet']
 
-    def validation(self, roll1, roll2):
-        totalFactor = 1
-        for bet in self.betList:
-            query = bet['query']
-            message = bet['message']
-            factor = bet['factor']
-            message = f'the player {self.id} wid: {query}, {message}'
-            if query == 1:
-                if roll1 % 2 == 0 and roll2 % 2 == 0:
-                    totalFactor *= factor
 
-            if query == 2:
-                if roll1 % 2 != 0 and roll2 % 2 != 0:
-                    totalFactor *= factor
-            if query == 3:
-                if roll1 % 2 == 0 or roll2 % 2 == 0:
-                    totalFactor *= factor
-
-            if query == 4:
-                if roll1 % 2 != 0 or roll2 % 2 != 0:
-                    totalFactor *= factor
-
-            if query == 5:
-                if roll1 % 2 == 0:
-                    totalFactor *= factor
-
-            if query == 6:
-                if roll2 % 2 == 0:
-                    totalFactor *= factor
-
-            if query == 7:
-                if roll1 % 2 != 0:
-                    totalFactor *= factor
-
-            if query == 8:
-                if roll2 % 2 != 0:
-                    totalFactor *= factor
-
-            if query == 9:
-                if (roll1 + roll2) % 2 == 0:
-                    totalFactor *= factor
-
-            if query == 10:
-                if (roll1 + roll2) % 2 != 0:
-                    totalFactor *= factor
-
-        self.amount['wallet'] = self.amount['wallet'] * totalFactor
 
