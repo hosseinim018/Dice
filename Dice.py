2ce7b46f4a4c3d7d7f522659eabe1a6dc67dc421
mahdi hosseini <hosseinim018@gmail.com>
2023-08-03 09:28:59 +0330

    update

--- Dice.py
+++ Dice.py
@@ -6,6 +6,7 @@ from Player import Player
 import re
 from random import randint
 
+
 def generate_dice():
     dices = {
         i: f'animateddice/tgs/{i}.tgs' for i in range(1, 7)
@@ -20,24 +21,9 @@ proxy = ("socks5", '127.0.0.1', 10808)
 
 Client = TelegramClient('Dice_session', api['api_id'], api['api_hash'], proxy=proxy).start(bot_token=api['bot_token'])
 
+
 @Client.on(events.NewMessage(pattern='/start'))
 async def start(event):
-    msg = "wellcome to the bot"
-    keyboard = [
-        [
-            Button.text('/start'),
-            Button.text('/test'),
-        ],
-        [
-            Button.text('/join'),
-            Button.text('/roll'),
-            Button.text('/wallet')
-        ]
-    ]
-    await event.respond(msg, buttons=keyboard)
-
-@Client.on(events.NewMessage(pattern='/join'))
-async def join(event):
     player = Player(event.chat_id)
     lobby.addPlayer(player)
     wallet = player.wallet()
@@ -45,28 +31,28 @@ async def join(event):
     betlist = ['minimumEven','minimumOdd','maximumEven','maximumOdd','roll1Even','roll2Even','sumEven']
     # bet.encode('utf-8')
     keyboard = [[Button.inline(bet , f"betlist-{bet}".encode('utf-8'))] for bet in betlist]
+    keyboard.append([Button.inline('play', b'play')])
     await event.respond(f'the number of players is: {len(lobby.players)}')
     await event.respond(f'the total amount of money player {player.id} is ${wallet} with bet with amount {bet}\nchoose your bets', buttons=keyboard)
 
-@Client.on(events.NewMessage(pattern='/roll'))
-async def roll_dice(event):
+
+async def play(event):
     lobby_amount = lobby.amount()
     dice_number1, file_path1 = generate_dice()
     dice_number2, file_path2 = generate_dice()
     if len(lobby.players) >= 2:
         for player in lobby.players:
-
-            rolls = (dice_number1, dice_number2)
-            lobby.set_rolls(rolls)
-
-            await Client.send_message(player.id, file=file_path1)
-            await Client.send_message(player.id, file=file_path2)
-            await Client.send_message(player.id, f"The dice1 rolled: {dice_number1}\nThe dice2 rolled: {dice_number2}")
-            await Client.send_message(player.id, f"the amount of lobby is: {lobby_amount}")
             # print(player.betList)
             if len(player.betList) == 0:
-                await event.respond('you should set a bet List. press /join')
+                await event.respond('you should set a bet List.')
             else:
+                rolls = (dice_number1, dice_number2)
+                lobby.set_rolls(rolls)
+
+                await Client.send_message(player.id, file=file_path1)
+                await Client.send_message(player.id, file=file_path2)
+                await Client.send_message(player.id, f"The dice1 rolled: {dice_number1}\nThe dice2 rolled: {dice_number2}")
+                await Client.send_message(player.id, f"the amount of lobby is: {lobby_amount}")
                 lobby.pay()
                 wallet = player.wallet()
                 await Client.send_message(player.id, f'now the total amount of money player {player.id} is ${wallet}.')
@@ -101,6 +87,7 @@ async def handle_callback_query(event):
                 msg = f"you secelet this {bet_id} bet"
                 await event.respond(msg)
                 player.set_betlist(bet_id)
-
+    if data == 'play':
+        await play(event)
 with Client:
     Client.run_until_disconnected()
